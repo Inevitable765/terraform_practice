8cd13db486e5edc716be9107e41eff9fce43d327
kushal meena <115863530+Inevitable765@users.noreply.github.com>
2026-05-27 22:14:52 +0530

    Fix formatting and structure in main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
-provider "aws' {
+provider "aws" {
   region = "ap-south-1"
 }
 
-# create s3 bucket -1
 resource "aws_s3_bucket" "example" {
   bucket = "inevitable8765-${terraform.workspace}"
 
   tags = {
-  Name =   "MyBucket-${terraform.workspace}"
-  Environment = terraform.workspace
- }
+    Name        = "MyBucket-${terraform.workspace}"
+    Environment = terraform.workspace
+  }
 }
